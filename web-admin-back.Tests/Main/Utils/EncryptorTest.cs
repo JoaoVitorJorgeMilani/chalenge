fbab31e57b918ed381f3d915b9cb74627a1d99fe
JoaoVitorJorgeMilani <joaovjmilani@gmail.com>
2024-05-03 21:47:37 -0300

    added EncryptorTests

--- web-admin-back.Tests/Main/Utils/EncryptorTest.cs
+++ web-admin-back.Tests/Main/Utils/EncryptorTest.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using MongoDB.Bson;
-using Xunit;
 using Main.Utils;
 
 public class EncryptorTest
@@ -13,7 +12,7 @@ public class EncryptorTest
     }
 
     [Fact]
-    public void Encryptor_Encrypt_ReturnEncryptedString()
+    public void Encryptor_Encrypt_WithObjectId_ReturnEncryptedString()
     {
         // Arrange
         ObjectId id = ObjectId.GenerateNewId();
@@ -26,5 +25,56 @@ public class EncryptorTest
         result.Should().BeOfType<string>();
         result.Length.Should().BeGreaterThan(10);
     }
-    
+
+    [Fact]
+    public void Encryptor_Encrypt_WithString_ReturnEncryptedString()
+    {
+        // Arrange
+        string str = "asdfqwe1234!@#$!@#!@$%%&%$#{}?";
+        
+        // Act
+        var result = _sutEncryptor.Encrypt(str);
+
+        // Assert
+        result.Should().NotBeNullOrEmpty();
+        result.Should().BeOfType<string>();
+        result.Length.Should().BeGreaterThan(10);
+    }
+
+    [Fact]
+    public void Encryptor_Decrypt_ReturnString()
+    {
+        // Arrange
+        string str = "asdfqwe1234!@#$!@#!@$%%&%$#{}?";
+        string encrypted = _sutEncryptor.Encrypt(str);
+        
+        // Act
+        var result = _sutEncryptor.Decrypt(encrypted);
+
+        // Assert
+        result.Should().NotBeNullOrEmpty();
+        result.Should().BeOfType<string>();
+        result.Length.Should().BeGreaterThan(10);
+        result.Should().Be(str);
+    }
+
+    [Fact]
+    public void Encryptor_DecryptObjectId_ReturnObjectId()
+    {
+        // Arrange
+        string id = ObjectId.GenerateNewId().ToString();
+        string encrypted = _sutEncryptor.Encrypt(id);
+        
+        // Act
+        var result = _sutEncryptor.DecryptObjectId(encrypted);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<ObjectId>();
+        result.Should().Be(ObjectId.Parse(id));
+        
+    }
+
+
+
 }
